87f2390566fe08b1bf729a5318dc95d45c2c5c34
Matt Schmitz <matt@0x2142.com>
2023-09-19 16:09:08 -0400

    Migrate discord messages to use embeds

--- notifier/discord.go
+++ notifier/discord.go
@@ -25,7 +25,6 @@ func SetupDiscord() {
 
 // SendDiscordMessage pushes alert message to Discord via webhook
 func SendDiscordMessage(message string, snapshot io.Reader) {
-	var file *discord.File
 	var err error
 
 	title := fmt.Sprintf("**%v**\n\n", AlertTitle)
@@ -33,11 +32,14 @@ func SendDiscordMessage(message string, snapshot io.Reader) {
 
 	// Send alert & attach snapshot if one was saved
 	if snapshot != nil {
-		file = discord.NewFile("snapshot.jpg", "", snapshot)
-		_, err = client.CreateMessage(discord.NewWebhookMessageCreateBuilder().SetContent(message).SetFiles(file).Build())
+		image := discord.NewFile("snapshot.jpg", "", snapshot)
+		embed := discord.NewEmbedBuilder().SetDescription(message).SetTitle(title).SetImage("attachment://snapshot.jpg").SetColor(5793266).Build()
+		_, err = client.CreateMessage(discord.NewWebhookMessageCreateBuilder().SetEmbeds(embed).SetFiles(image).Build())
 	} else {
 		message += "\nNo snapshot saved."
-		_, err = client.CreateMessage(discord.NewWebhookMessageCreateBuilder().SetContent(message).Build())
+		embed := discord.NewEmbedBuilder().SetDescription(message).SetTitle(title).SetColor(5793266).Build()
+		_, err = client.CreateMessage(discord.NewWebhookMessageCreateBuilder().SetEmbeds(embed).Build())
+
 	}
 	if err != nil {
 		log.Printf("Unable to send Discord Alert: %v", err)
